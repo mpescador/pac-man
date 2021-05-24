e3375d9ab3ab5fa44d8d2f3776228f3a73b60a0c
Juan Pablo <juanpablo272394@gmail.com>
2021-05-24 15:34:17 +0100

    contador de monedas

--- Project_Thief_Runner/src/index.js
+++ Project_Thief_Runner/src/index.js
@@ -4,11 +4,13 @@ const Thief = function () {
   this.posx = 9
   this.posy = 9
   this.direction = 1 // 1-up, 2-right, 3-down, 4-left
-}
 
+}
+let moneyCounter = 0
 const thief = new Thief()
 const arrR = [[0, 19, 0], [0, 19, 18], [0,4,2], [5,17,2], [3,5,4], [0,11,16], [3,5,13], [6,11,4], [6,11,14], [12,16,4], [12,16,15], [8,11,6], [8,11,12] ]
 const arrC = [[0, 19, 0], [0, 19, 18], [6,12,2], [5,13,4], [5,14,6], [7,12,10], [5,15,12], [6,14,14], [6,14,16]]
+board[8][3] = 3
 
 // funciones
 const wallsGeneratorC = function () {
@@ -43,7 +45,7 @@ const changeDirection = function (code) {
   if (code === 'ArrowDown') this.direction = 3
   if (code === 'ArrowLeft') this.direction = 4
 }
-// 0 = path 1 = wall 2 = thief
+// 0 = path 1 = wall 2 = thief 3 = money
 const printBoard = function () {
   board.forEach(function (row, r) {
     row.forEach(function (col, c) {
@@ -52,7 +54,7 @@ const printBoard = function () {
       if (board[r][c] === 2) {
         colSelect.classList.add('thief')
       } else if (board[r][c] === 0) {
-        colSelect.classList.remove('thief')
+        colSelect.classList.remove('thief', 'money')
       }
     })
   })
@@ -61,19 +63,31 @@ const printBoard = function () {
 const moveThief = function () {
   // 1-up, 2-right, 3-down, 4-left
   if (this.direction === 1 && board[thief.posy - 1][thief.posx] !== 1) {
-    console.log(board[thief.posy - 1][thief.posx])
+    if (board[thief.posy - 1][thief.posx] === 3) {
+      moneyCounter++
+    }
     board[thief.posy][thief.posx] = 0
     thief.posy--
   } else if (this.direction === 2 && board[thief.posy][thief.posx + 1] !== 1) {
+    if (board[thief.posy][thief.posx + 1] === 3) {
+      moneyCounter++
+    }
     board[thief.posy][thief.posx] = 0
     thief.posx++
   } else if (this.direction === 3 && board[thief.posy + 1][thief.posx] !== 1) {
+    if (board[thief.posy + 1][thief.posx] === 3) {
+      moneyCounter++
+    }
     board[thief.posy][thief.posx] = 0
     thief.posy++
   } else if (this.direction === 4 && board[thief.posy][thief.posx - 1] !== 1) {
+    if (board[thief.posy][thief.posx - 1] === 3) {
+      moneyCounter++
+    }
     board[thief.posy][thief.posx] = 0
     thief.posx--
   }
+  document.getElementById('moneyCounter').innerText = moneyCounter
   board[thief.posy][thief.posx] = 2
 }
 
@@ -85,7 +99,7 @@ const game = function () {
 
 // ejecucion del juego
 
-setInterval(game, 600)
+setInterval(game, 200)
 
 window.addEventListener('keydown', function (e) {
   changeDirection(e.code)
