3c773d030c3dcd4f8d980e13eb35214b2acf8c8c
Juan Pablo <juanpablo272394@gmail.com>
2021-05-24 16:10:56 +0100

    crear monedas en el mapa

--- Project_Thief_Runner/src/index.js
+++ Project_Thief_Runner/src/index.js
@@ -1,5 +1,4 @@
-const board = Array(19).fill(0).map(() => Array(19).fill(0))
-console.log(board)
+let board = Array(19).fill(0).map(() => Array(19).fill(0))
 const Thief = function () {
   this.posx = 9
   this.posy = 9
@@ -11,6 +10,7 @@ const thief = new Thief()
 const arrR = [[0, 19, 0], [0, 19, 18], [0,4,2], [5,17,2], [3,5,4], [0,11,16], [3,5,13], [6,11,4], [6,11,14], [12,16,4], [12,16,15], [8,11,6], [8,11,12] ]
 const arrC = [[0, 19, 0], [0, 19, 18], [6,12,2], [5,13,4], [5,14,6], [7,12,10], [5,15,12], [6,14,14], [6,14,16]]
 board[8][3] = 3
+
 
 // funciones
 const wallsGeneratorC = function () {
@@ -29,7 +29,6 @@ const wallsGeneratorC = function () {
 const wallsGeneratorR = function () {
   for (let i = 0; i < arrC.length; i++) {
     let cuenta = arrC[i][0]
-    console.log(arrC)
     for (let j = arrC[i][0]; j < arrC[i][1]; j++) {
       const rowSelect = document.querySelector(`.row${arrC[i][2] + 1}`)
       const colSelect = rowSelect.querySelector(`.col${cuenta + 1}`)
@@ -55,6 +54,9 @@ const printBoard = function () {
         colSelect.classList.add('thief')
       } else if (board[r][c] === 0) {
         colSelect.classList.remove('thief', 'money')
+      }
+      else if (board[r][c] === 3) {
+        colSelect.classList.add('money')
       }
     })
   })
@@ -94,7 +96,6 @@ const moveThief = function () {
 const game = function () {
   moveThief()
   printBoard()
-  console.log(thief.posy)
 }
 
 // ejecucion del juego
@@ -107,3 +108,16 @@ window.addEventListener('keydown', function (e) {
 
 wallsGeneratorR()
 wallsGeneratorC()
+let newBoard = []
+
+board.forEach(function (row, r) {
+  newBoard.push(row.map(function (col, c) {
+    if (col === 0) {
+      return col + 3
+    } else {
+      return col
+    }
+  }))
+})
+board = newBoard
+console.log(board)
