57c773655eee9651cf4969fe079b3a3756334544
JuanPabloLeberPadilla <77406815+JuanPablo272394@users.noreply.github.com>
2021-05-28 16:21:35 +0100

    Update index.js
    teleport

--- Project_Thief_Runner/src/index.js
+++ Project_Thief_Runner/src/index.js
@@ -4,7 +4,7 @@ const Thief = function () {
   this.posy = 8
   this.direction = -1 // 1-up, 2-right, 3-down, 4-left
 }
-const Ghost = function (x, y) {
+const Cop = function (x, y) {
   this.posx = x
   this.posy = y
   this.direction = 1
@@ -21,15 +21,16 @@ let newBoard = []
 const audio = new Audio('src/assets/sounds/music1.mp3')
 let interval
 const thief = new Thief()
-const ghost1 = new Ghost(1, 1)
-const ghost2 = new Ghost(1, 17)
-const ghost3 = new Ghost(17, 17)
-const ghost4 = new Ghost(17, 1)
+const cop1 = new Cop(1, 1)
+const cop2 = new Cop(1, 17)
+const cop3 = new Cop(17, 17)
+const cop4 = new Cop(17, 1)
+//const cop5 = new Cop(16, 1)
 let lvl = 1
 let speed = 300
 
 const arrR = [[0, 19, 0], [0, 19, 18], [2, 6, 2], [8, 11, 2], [8, 11, 4], [8, 11, 6], [13, 16, 6], [12, 13, 5], [15, 16, 5], [13, 16, 4], [2, 6, 9], [2, 6, 13], [7, 11, 9], [7, 11, 13], [12, 17, 9], [2, 17, 15], [5, 14, 16], [8, 11, 17]]
-const arrC = [[0, 19, 0], [0, 19, 18], [3, 7, 2], [3, 5, 5], [2, 7, 7], [2, 7, 12], [9, 14, 2], [9, 14, 5], [9, 14, 7], [9, 14, 10], [9, 14, 14], [4, 7, 4]]
+const arrC = [[0, 7, 0], [8, 19, 0], [0, 7, 18], [8, 18, 18], [3, 7, 2], [3, 5, 5], [2, 7, 7], [2, 7, 12], [9, 14, 2], [9, 14, 5], [9, 14, 7], [9, 14, 10], [9, 14, 14], [4, 7, 4]]
 
 const arrR2 = [[0, 19, 0], [0, 19, 18], [2, 5, 2], [6, 10, 2], [11, 17, 2], [2, 5, 4], [0, 5, 16], [8, 11, 16], [2, 5, 14], [6, 11, 4], [8, 11, 14], [12, 17, 4], [14, 17, 16], [8, 11, 6], [8, 11, 12]]
 const arrC2 = [[0, 19, 0], [0, 19, 18], [6, 9, 2], [10, 13, 2], [5, 9, 4], [10, 13, 4], [5, 8, 6], [9, 15, 6], [7, 12, 10], [5, 10, 12], [11, 17, 12], [16, 18, 6], [6, 9, 14], [10, 15, 14], [6, 9, 16], [10, 15, 16], [8, 11, 8]]
@@ -84,7 +85,7 @@ const changeDirection = function (code) {
   if (code === 'ArrowDown') thief.direction = 3
   if (code === 'ArrowLeft') thief.direction = 4
 }
-// 0 = path 1 = wall 2 = thief 3 = money 4 = ghost
+// 0 = path 1 = wall 2 = thief 3 = money 4 = cop
 const printBoard = function () {
   board.forEach(function (row, r) {
     row.forEach(function (col, c) {
@@ -92,24 +93,24 @@ const printBoard = function () {
       const colSelect = rowSelect.querySelector(`.col${c + 1}`)
       if (board[r][c] === 2) {
         colSelect.classList.add('thief')
-        colSelect.classList.remove('money', 'ghost')
+        colSelect.classList.remove('money', 'cop')
       } else if (board[r][c] === 0) {
-        colSelect.classList.remove('thief', 'money', 'ghost', 'walls')
+        colSelect.classList.remove('thief', 'money', 'cop', 'walls')
       } else if (board[r][c] === 3) {
         colSelect.classList.add('money')
-        colSelect.classList.remove('ghost', 'thief', 'walls')
+        colSelect.classList.remove('cop', 'thief', 'walls')
       } else if (board[r][c] === 4) {
-        colSelect.classList.add('ghost')
+        colSelect.classList.add('cop')
         colSelect.classList.remove('thief', 'money')
       } else if (board[r][c] === 1) {
         colSelect.classList.add('walls')
-        colSelect.classList.remove('money', 'thief', 'ghost')
+        colSelect.classList.remove('money', 'thief', 'cop')
       }
     })
   })
 }
 
-const checkCell = function (colisionUp, colisionRight, colisionDown, colisionLeft, ghost) {
+const checkCell = function (colisionUp, colisionRight, colisionDown, colisionLeft, cop) {
   const freePos = []
   if (colisionUp !== 1) {
     freePos.push(1)
@@ -124,93 +125,105 @@ const checkCell = function (colisionUp, colisionRight, colisionDown, colisionLef
     freePos.push(4)
   }
 
-  if (ghost.contCell !== 3) {
-    ghost.contCell++
+  if (cop.contCell !== 3) {
+    cop.contCell++
     for (let i = 0; i < freePos.length; i++) {
-      if (ghost.direction === freePos[i]) return freePos[i]
+      if (cop.direction === freePos[i]) return freePos[i]
     }
   } else {
     const indexRandom = Math.random() * (freePos.length)
     const indexFloor = Math.floor(indexRandom)
-    ghost.contCell = 0
+    cop.contCell = 0
     return freePos[indexFloor]
   }
 }
 
-const colisionUpGhost = function (colisionUp, ghost) {
-  if (board[ghost.posy - 1][ghost.posx] === 2) {
+const colisionUpCop = function (colisionUp, cop) {
+  if (board[cop.posy - 1][cop.posx] === 2) {
     lifesCounter--
     board[thief.posy][thief.posx] = 0
     thief.posy = POSY
     thief.posx = POSX
     thief.direction = -1
-    soundColisionGhost()
+    soundColisionCop()
   } else {
-    board[ghost.posy][ghost.posx] = ghost.nextCell
-    ghost.nextCell = colisionUp === 4 ? ghost.nextCell : colisionUp
-    ghost.posy--
+    board[cop.posy][cop.posx] = cop.nextCell
+    cop.nextCell = colisionUp === 4 ? cop.nextCell : colisionUp
+    cop.posy--
   }
 }
-const colisionRightGhost = function (colisionRight, ghost) {
-  if (board[ghost.posy][ghost.posx + 1] === 2) {
+const colisionRightCop = function (colisionRight, cop) {
+  if (board[cop.posy][cop.posx + 1] === 2) {
     lifesCounter--
     board[thief.posy][thief.posx] = 0
     thief.posy = POSY
     thief.posx = POSX
     thief.direction = -1
-    soundColisionGhost()
+    soundColisionCop()
   } else {
-    board[ghost.posy][ghost.posx] = ghost.nextCell
-    ghost.nextCell = colisionRight === 4 ? ghost.nextCell : colisionRight
-    ghost.posx++
+    board[cop.posy][cop.posx] = cop.nextCell
+    cop.nextCell = colisionRight === 4 ? cop.nextCell : colisionRight
+    cop.posx++
+  }
+  if (cop.posx === 18 && cop.posy === 7) {
+    console.log('if2')
+    board[cop.posy][cop.posx] = 0
+    cop.posx = 0
+    cop.posy = 7
   }
 }
-const colisionDownGhost = function (colisionDown, ghost) {
-  if (board[ghost.posy + 1][ghost.posx] === 2) {
+const colisionDownCop = function (colisionDown, cop) {
+  if (board[cop.posy + 1][cop.posx] === 2) {
     lifesCounter--
     board[thief.posy][thief.posx] = 0
     thief.posy = POSY
     thief.posx = POSX
     thief.direction = -1
-    soundColisionGhost()
+    soundColisionCop()
   } else {
-    board[ghost.posy][ghost.posx] = ghost.nextCell
-    ghost.nextCell = colisionDown === 4 ? ghost.nextCell : colisionDown
-    ghost.posy++
+    board[cop.posy][cop.posx] = cop.nextCell
+    cop.nextCell = colisionDown === 4 ? cop.nextCell : colisionDown
+    cop.posy++
   }
 }
-const colisionLeftGhost = function (colisionLeft, ghost) {
-  if (board[ghost.posy][ghost.posx - 1] === 2) {
+const colisionLeftCop = function (colisionLeft, cop) {
+  if (board[cop.posy][cop.posx - 1] === 2) {
     lifesCounter--
     board[thief.posy][thief.posx] = 0
     thief.posy = POSY
     thief.posx = POSX
     thief.direction = -1
-    soundColisionGhost()
+    soundColisionCop()
   } else {
-    board[ghost.posy][ghost.posx] = ghost.nextCell
-    ghost.nextCell = colisionLeft === 4 ? ghost.nextCell : colisionLeft
-    ghost.posx--
+    board[cop.posy][cop.posx] = cop.nextCell
+    cop.nextCell = colisionLeft === 4 ? cop.nextCell : colisionLeft
+    cop.posx--
+  }
+  if (cop.posx === 0 && cop.posy === 7) {
+    console.log(board)
+    board[cop.posy][cop.posx] = 0
+    cop.posx = 18
+    cop.posy = 7
   }
 }
-const moveGhost = function (ghost) {
-  const colisionUp = board[ghost.posy - 1][ghost.posx]
-  const colisionRight = board[ghost.posy][ghost.posx + 1]
-  const colisionDown = board[ghost.posy + 1][ghost.posx]
-  const colisionLeft = board[ghost.posy][ghost.posx - 1]
+const moveCop = function (cop) {
+  const colisionUp = board[cop.posy - 1][cop.posx]
+  const colisionRight = board[cop.posy][cop.posx + 1]
+  const colisionDown = board[cop.posy + 1][cop.posx]
+  const colisionLeft = board[cop.posy][cop.posx - 1]
 
-  ghost.direction = checkCell(colisionUp, colisionRight, colisionDown, colisionLeft, ghost)
+  cop.direction = checkCell(colisionUp, colisionRight, colisionDown, colisionLeft, cop)
   // 1-up, 2-right, 3-down, 4-left
-  if (ghost.direction === 1 && colisionUp !== 1) {
-    colisionUpGhost(colisionUp, ghost)
-  } else if (ghost.direction === 2 && colisionRight !== 1) {
-    colisionRightGhost(colisionRight, ghost)
-  } else if (ghost.direction === 3 && colisionDown !== 1) {
-    colisionDownGhost(colisionDown, ghost)
-  } else if (ghost.direction === 4 && colisionLeft !== 1) {
-    colisionLeftGhost(colisionLeft, ghost)
+  if (cop.direction === 1 && colisionUp !== 1) {
+    colisionUpCop(colisionUp, cop)
+  } else if (cop.direction === 2 && colisionRight !== 1) {
+    colisionRightCop(colisionRight, cop)
+  } else if (cop.direction === 3 && colisionDown !== 1) {
+    colisionDownCop(colisionDown, cop)
+  } else if (cop.direction === 4 && colisionLeft !== 1) {
+    colisionLeftCop(colisionLeft, cop)
   }
-  board[ghost.posy][ghost.posx] = 4
+  board[cop.posy][cop.posx] = 4
 }
 
 const moveThiefUp = function (colisionUp) {
@@ -220,7 +233,7 @@ const moveThiefUp = function (colisionUp) {
     thief.posy = POSY
     thief.posx = POSX
     thief.direction = -1
-    soundColisionGhost()
+    soundColisionCop()
   }
   if (colisionUp === 3) {
     moneyCounter++
@@ -239,7 +252,7 @@ const moveThiefRight = function (colisionRight) {
     thief.posy = POSY
     thief.posx = POSX
     thief.direction = -1
-    soundColisionGhost()
+    soundColisionCop()
   }
   if (colisionRight === 3) {
     moneyCounter++
@@ -247,8 +260,15 @@ const moveThiefRight = function (colisionRight) {
     thief.posx++
   }
   if (colisionRight === 0) {
+    console.log('if1')
     board[thief.posy][thief.posx] = 0
     thief.posx++
+  }
+  if (thief.posx === 18 && thief.posy === 7) {
+    console.log('if2')
+    board[thief.posy][thief.posx] = 0
+    thief.posx = 0
+    thief.posy = 7
   }
 }
 const moveThiefDown = function (colisionDown) {
@@ -258,7 +278,7 @@ const moveThiefDown = function (colisionDown) {
     thief.posy = POSY
     thief.posx = POSX
     thief.direction = -1
-    soundColisionGhost()
+    soundColisionCop()
   }
   if (colisionDown === 3) {
     moneyCounter++
@@ -277,7 +297,7 @@ const moveThiefLeft = function (colisionLeft) {
     thief.posy = POSY
     thief.posx = POSX
     thief.direction = -1
-    soundColisionGhost()
+    soundColisionCop()
   }
   if (colisionLeft === 3) {
     moneyCounter++
@@ -287,6 +307,12 @@ const moveThiefLeft = function (colisionLeft) {
   if (colisionLeft === 0) {
     board[thief.posy][thief.posx] = 0
     thief.posx--
+  }
+  if (thief.posx === 0 && thief.posy === 7) {
+    console.log(board)
+    board[thief.posy][thief.posx] = 0
+    thief.posx = 18
+    thief.posy = 7
   }
 }
 
@@ -382,18 +408,18 @@ const defaultValues = function () {
     lvl = 1
     speed = 300
   }
-  ghost1.posx = 1
-  ghost1.posy = 1
-  ghost2.posx = 1
-  ghost2.posy = 17
-  ghost3.posx = 17
-  ghost3.posy = 17
-  ghost4.posx = 17
-  ghost4.posy = 1
-  ghost1.contCell = 0
-  ghost2.contCell = 0
-  ghost3.contCell = 0
-  ghost4.contCell = 0
+  cop1.posx = 1
+  cop1.posy = 1
+  cop2.posx = 1
+  cop2.posy = 17
+  cop3.posx = 17
+  cop3.posy = 17
+  cop4.posx = 17
+  cop4.posy = 1
+  cop1.contCell = 0
+  cop2.contCell = 0
+  cop3.contCell = 0
+  cop4.contCell = 0
   moneyCounter = 0
   lifesCounter = 2
 
@@ -401,27 +427,34 @@ const defaultValues = function () {
   thief.posx = POSX
   thief.direction = -1
 }
-function level (arrrows, arrcolum) {
+function level(arrrows, arrcolum) {
   defaultValues()
   wallsGeneratorR(arrrows)
   wallsGeneratorC(arrcolum)
   coinGenerator()
   fillBoard()
   clearInterval(interval)
+  // transport
+  board[7][18] = 0
+  board[7][0] = 0
   interval = setInterval(game, speed)
 }
-function startGame () {
+function startGame() {
+
   let modal = document.getElementById('myModalGameOver')
   modal.style.display = 'none'
   let modal1 = document.getElementById('myModalWin')
   modal1.style.display = 'none'
   newBoard = []
   board = Array(19).fill(0).map(() => Array(19).fill(0))
+  printBoard()
   wallsGeneratorR(arrR)
   wallsGeneratorC(arrC)
   coinGenerator()
   let lvlGame = document.getElementById('lvl')
   lvlGame.innerText = 1
+  lvl = 1
+  speed = 300
   defaultValues()
   fillBoard()
   board[5][14] = 0
@@ -438,7 +471,9 @@ function startGame () {
   board[10][9] = 0
   board[12][8] = 0
   board[12][9] = 0
-  console.log(board)
+  // transport
+  board[7][18] = 0
+  board[7][0] = 0
   clearInterval(interval)
   audio.play()
   interval = setInterval(game, speed)
@@ -446,10 +481,10 @@ function startGame () {
 // ejecucion del juego
 const game = function () {
   moveThief()
-  moveGhost(ghost1)
-  moveGhost(ghost2)
-  moveGhost(ghost3)
-  moveGhost(ghost4)
+  moveCop(cop1)
+  moveCop(cop2)
+  moveCop(cop3)
+  moveCop(cop4)
   winLvl()
   winCondition()
   loseCondition()
@@ -459,9 +494,9 @@ const game = function () {
 const btnStartGame = document.getElementById('btnPlay')
 btnStartGame.addEventListener('click', startGame)
 
-function soundColisionGhost() {
-  const audioColisionGhost = new Audio('src/assets/sounds/choquefantasma.mp3')
-  audioColisionGhost.play()
+function soundColisionCop() {
+  const audioColisionCop = new Audio('src/assets/sounds/choquefantasma.mp3')
+  audioColisionCop.play()
 }
 window.onload = function () {
   const windowIni = document.getElementById('modalIniGame')
